e54c626c0fc0423f16e59f1b98cbf0d2ea99f6e7
DHDC Main <utehnphnu2015@gmail.com>
2015-06-15 23:29:45 +0700

    template complate,utehn

---
+++ controllers/AjaxController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace app\controllers;
+
+class AjaxController extends \yii\web\Controller
+{
+    public function actionListAmp($p=null)
+    {
+        
+    }
+    
+    public function actionListHos($p=null,$a=null)
+    {
+        
+    }
+
+}
