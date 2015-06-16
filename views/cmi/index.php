d7b31d15b09c8b399cbde575a166393dbc015fce
DHDC Main <utehnphnu2015@gmail.com>
2015-06-16 23:32:35 +0700

    template complate,utehn

--- views/cmi/index.php
+++ views/cmi/index.php
@@ -12,7 +12,7 @@ $this->params['breadcrumbs'][] = 'CMI';
 <!-- Default box -->
 <div class="box">
     <div class="box-header with-border">
-        <h3 class="box-title"><i class="glyphicon glyphicon-th-list"></i> ตัวชี้วัด CMI</h3>
+        <h3 class="box-title"><i class="glyphicon glyphicon-th-list"></i> CMI</h3>
         <div class="box-tools pull-right">
             <button class="btn btn-box-tool" data-widget="collapse" data-toggle="tooltip" title="Collapse">
                 <i class="fa fa-minus"></i>
