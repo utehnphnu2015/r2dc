06de782338d3ef6aa0126bb729dd710a0328377f
Knott <Knott@192.168.187.209>
2015-06-16 22:53:24 +0700

    knott,2015-06-16

--- controllers/CmiController.php
+++ controllers/CmiController.php
@@ -13,10 +13,7 @@ class CmiController extends \yii\web\Controller {
     }
 
     public function actionIndex($rep_year = 2015) {// แสดงทุกรายการ kpi เขต
-        $sql = "SELECT k.rep_year,c.provname,c.hospcode,c.hospname,k.sumcase,k.sumadjrw,k.refcmi
-FROM kpi_cmi k,chos c
-where k.hospcode=c.hospcode and k.rep_year=$rep_year
-ORDER BY k.provcode";
+        $sql = "SELECT * from tmp_kpi_cmi where rep_year=$rep_year";
         $raw = $this->queryAll($sql);
         $dataProvider = new ArrayDataProvider([
             'allModels' => $raw
