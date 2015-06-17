4f34f210e9329c6e0e901d6e1251689265378d86
DHDC Main <utehnphnu2015@gmail.com>
2015-06-17 23:23:23 +0700

    template complate,utehn

--- views/site/index.php
+++ views/site/index.php
@@ -6,9 +6,12 @@ $this->registerJsFile(Yii::$app->request->baseUrl . '/js/dashboard2.js', [
 <!-- Info boxes -->
 <div class="row">
     <?php
-    $url_moph = Yii::$app->urlManager->createUrl(['moph/index']);
+    $url1 = Yii::$app->urlManager->createUrl(['moph/index']);
+    $url2 = Yii::$app->urlManager->createUrl(['region/index']);
+    $url3 = Yii::$app->urlManager->createUrl(['qof/index']);
+    $url4 = Yii::$app->urlManager->createUrl(['cmi/index']);
     ?>
-    <div class="col-md-3 col-sm-6 col-xs-12" style="cursor: pointer" onclick="location.href='<?=$url_moph?>'">
+    <div class="col-md-3 col-sm-6 col-xs-12" style="cursor: pointer" onclick="location.href='<?=$url1?>'">
         
         <div class="info-box">
             <span class="info-box-icon bg-aqua"><i class="ion ion-ios-gear-outline"></i></span>
@@ -19,7 +22,7 @@ $this->registerJsFile(Yii::$app->request->baseUrl . '/js/dashboard2.js', [
         </div><!-- /.info-box -->
        
     </div><!-- /.col -->
-    <div class="col-md-3 col-sm-6 col-xs-12">
+    <div class="col-md-3 col-sm-6 col-xs-12" style="cursor: pointer" onclick="location.href='<?=$url2?>'">
         <div class="info-box">
             <span class="info-box-icon bg-red"><i class="fa fa-building-o"></i></span>
             <div class="info-box-content">
@@ -32,7 +35,7 @@ $this->registerJsFile(Yii::$app->request->baseUrl . '/js/dashboard2.js', [
     <!-- fix for small devices only -->
     <div class="clearfix visible-sm-block"></div>
 
-    <div class="col-md-3 col-sm-6 col-xs-12">
+    <div class="col-md-3 col-sm-6 col-xs-12" style="cursor: pointer" onclick="location.href='<?=$url3?>'">
         <div class="info-box">
             <span class="info-box-icon bg-green"><i class="ion ion-ios-cart-outline"></i></span>
             <div class="info-box-content">
@@ -41,7 +44,7 @@ $this->registerJsFile(Yii::$app->request->baseUrl . '/js/dashboard2.js', [
             </div><!-- /.info-box-content -->
         </div><!-- /.info-box -->
     </div><!-- /.col -->
-    <div class="col-md-3 col-sm-6 col-xs-12">
+    <div class="col-md-3 col-sm-6 col-xs-12" style="cursor: pointer" onclick="location.href='<?=$url4?>'">
         <div class="info-box">
             <span class="info-box-icon bg-yellow"><i class="ion ion-ios-people-outline"></i></span>
             <div class="info-box-content">
