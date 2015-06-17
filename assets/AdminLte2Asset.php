e0f481b5b4245f1f9d062aeb83cc339b56bf3074
DHDC Main <utehnphnu2015@gmail.com>
2015-06-17 22:45:07 +0700

    template complate,utehn

--- assets/AdminLte2Asset.php
+++ assets/AdminLte2Asset.php
@@ -14,13 +14,19 @@ class AdminLte2Asset extends AssetBundle
         'css/AdminLTE.min.css',
         'css/skins/_all-skins.min.css',
         'ionicons/css/ionicons.min.css',
-        'font-awesome/css/font-awesome.min.css'
+        'font-awesome/css/font-awesome.min.css',
+        "plugins/jvectormap/jquery-jvectormap-1.2.2.css"
     ];
     public $js = [
         'js/app.min.js',
         'js/demo.js',
         'plugins/slimScroll/jquery.slimscroll.min.js',
-        'plugins/fastclick/fastclick.min.js'
+        'plugins/fastclick/fastclick.min.js',
+        'plugins/chartjs/Chart.min.js',
+        "plugins/sparkline/jquery.sparkline.min.js",
+        //"js/pages/dashboard2.js" ,
+        "plugins/jvectormap/jquery-jvectormap-1.2.2.min.js",
+        "plugins/jvectormap/jquery-jvectormap-world-mill-en.js"
     ];
     public $depends = [
         'yii\web\YiiAsset',
---
+++ views/site/index_1.php
@@ -0,0 +1,195 @@
+<?php
+
+use yii\helpers\Url;
+
+
+$command = Yii::$app->db->createCommand("SELECT COUNT(id) FROM  topic_all WHERE kpi_group='moph'");
+$moph = $command->queryScalar();
+$command = Yii::$app->db->createCommand("SELECT COUNT(id) FROM  topic_all WHERE kpi_group='region'");
+$region = $command->queryScalar();
+//$command = Yii::$app->db->createCommand('SELECT COUNT(id) AS tcount FROM topic_province');
+//$province = $command->queryScalar();
+$command = Yii::$app->db->createCommand("SELECT COUNT(id) FROM  topic_all WHERE kpi_group='qof'");
+$qof = $command->queryScalar();
+?>
+
+<div class="constraints">
+    <div class="row">
+        <div class="col-lg-3 col-xs-6">
+            <div class="small-box bg-aqua">
+                <div class="inner">
+                    <h3><?= $moph ?></h3>
+                    <p>ตัวชี้วัดกระทรวง</p>
+                </div>
+                <div class="icon">
+                    <i class="ion ion-medkit"></i>
+                </div>
+                <a href="<?php echo Url::to(['moph/index']); ?>" class="small-box-footer">รายละเอียด <i class="fa fa-arrow-circle-right"></i></a>
+
+            </div>
+        </div>
+
+        <div class="col-lg-3 col-xs-6">
+            <!-- small box -->
+            <div class="small-box bg-green">
+                <div class="inner">
+                    <h3><?= $region ?></h3>
+                    <p>ตัวชี้วัดเขต</p>
+                </div>
+                <div class="icon">
+                    <i class="ion ion-stats-bars"></i>
+                </div>
+                <a href="<?php echo Url::to(['region/index']); ?>" class="small-box-footer">รายละเอียด <i class="fa fa-arrow-circle-right"></i></a>
+            </div>
+        </div><!-- ./col -->
+        
+        <div class="col-lg-3 col-xs-6">
+            <!-- small box -->
+            <div class="small-box bg-red">
+                <div class="inner">
+                    <h3><?= $qof ?></h3>
+                    <p>ตัวชี้วัด QOF</p>
+                </div>
+                <div class="icon">
+                    <i class="ion ion-pie-graph"></i>
+                </div>
+                <a href="<?php echo Url::to(['qof/index']); ?>" class="small-box-footer">รายละเอียด<i class="fa fa-arrow-circle-right"></i></a>
+            </div>
+        </div><!-- ./col -->
+
+        <div class="col-lg-3 col-xs-6">
+            <!-- small box -->
+            <div class="small-box bg-yellow">
+                <div class="inner">
+                    <h3>47</h3>
+                    <p>CMI</p>
+                </div>
+                <div class="icon">
+                    <i class="ion ion-ios-book-outline"></i>
+                </div>
+                <a href="<?php echo Url::to(['cmi/index']); ?>" class="small-box-footer">รายละเอียด<i class="fa fa-arrow-circle-right"></i></a>
+            </div>
+        </div><!-- ./col -->
+        
+    </div>
+</div>
+
+
+<div class="constraints">
+    <div class="row">
+        <div class="col-lg-3 col-xs-6">
+            <div class="small-box bg-maroon">
+                <div class="inner">
+                    <h3>0</h3>
+                    <p>ข้อมูลทั่วไป</p>
+                </div>
+                <div class="icon">
+                    <i class="ion ion-bag"></i>
+                </div>
+                <a href="#" class="small-box-footer">รายละเอียด <i class="fa fa-arrow-circle-right"></i></a>
+
+            </div>
+        </div>
+
+        <div class="col-lg-3 col-xs-6">
+            <!-- small box -->
+            <div class="small-box bg-teal-active">
+                <div class="inner">
+                    <h3>0</h3>
+                    <p>กำลังคน</p>
+                </div>
+                <div class="icon">
+                    <i class="ion ion-person-stalker"></i>
+                </div>
+                <a href="#" class="small-box-footer">รายละเอียด <i class="fa fa-arrow-circle-right"></i></a>
+            </div>
+        </div><!-- ./col -->
+
+        <div class="col-lg-3 col-xs-6">
+            <!-- small box -->
+            <div class="small-box bg-olive">
+                <div class="inner">
+                    <h3>0</h3>
+                    <p>การเงินการคลัง</p>
+                </div>
+                <div class="icon">
+                    <i class="ion ion-erlenmeyer-flask-bubbles"></i>
+                </div>
+                <a href="#" class="small-box-footer">รายละเอียด<i class="fa fa-arrow-circle-right"></i></a>
+            </div>
+        </div><!-- ./col -->
+        <div class="col-lg-3 col-xs-6">
+            <!-- small box -->
+            <div class="small-box bg-light-blue-gradient">
+                <div class="inner">
+                    <h3>0</h3>
+                    <p>GIS</p>
+                </div>
+                <div class="icon">
+                    <i class="ion ion-android-pin"></i>
+                </div>
+                <a href="#" class="small-box-footer">รายละเอียด<i class="fa fa-arrow-circle-right"></i></a>
+            </div>
+        </div><!-- ./col -->
+    </div>
+</div>
+
+<div class="constraints">
+    <div class="row">
+        <div class="col-lg-3 col-xs-6">
+            <div class="small-box bg-fuchsia-active">
+                <div class="inner">
+                    <h3>0</h3>
+                    <p>ส่งข้อมูล</p>
+                </div>
+                <div class="icon">
+                    <i class="ion ion-android-upload"></i>
+                </div>
+                <a href="#" class="small-box-footer">รายละเอียด <i class="fa fa-arrow-circle-right"></i></a>
+
+            </div>
+        </div>
+
+        <div class="col-lg-3 col-xs-6">
+            <!-- small box -->
+            <div class="small-box bg-lime-active">
+                <div class="inner">
+                    <h3>0</h3>
+                    <p>สถิติ</p>
+                </div>
+                <div class="icon">
+                    <i class="ion ion-connection-bars"></i>
+                </div>
+                <a href="#" class="small-box-footer">รายละเอียด <i class="fa fa-arrow-circle-right"></i></a>
+            </div>
+        </div><!-- ./col -->
+
+        <div class="col-lg-3 col-xs-6">
+            <!-- small box -->
+            <div class="small-box bg-gray-active">
+                <div class="inner">
+                    <h3>0</h3>
+                    <p>อื่นๆ</p>
+                </div>
+                <div class="icon">
+                    <i class="ion ion-erlenmeyer-flask-bubbles"></i>
+                </div>
+                <a href="#" class="small-box-footer">รายละเอียด<i class="fa fa-arrow-circle-right"></i></a>
+            </div>
+        </div><!-- ./col -->
+        <div class="col-lg-3 col-xs-6">
+            <!-- small box -->
+            <div class="small-box bg-purple-active">
+                <div class="inner">
+                    <h3>0</h3>
+                    <p>Back Office</p>
+                </div>
+                <div class="icon">
+                    <i class="ion ion-android-settings"></i>
+                </div>
+                <a href="#" class="small-box-footer">รายละเอียด<i class="fa fa-arrow-circle-right"></i></a>
+            </div>
+        </div><!-- ./col -->
+    </div>
+</div>
+
