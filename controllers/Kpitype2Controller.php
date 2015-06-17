c9e6d48d83aa423ebce461826aec2ae1769c2349
DHDC Main <utehnphnu2015@gmail.com>
2015-06-17 10:45:40 +0700

    template complate,utehn

--- controllers/Kpitype2Controller.php
+++ controllers/Kpitype2Controller.php
@@ -14,6 +14,7 @@ use yii\filters\VerbFilter;
  */
 class Kpitype2Controller extends Controller {
 
+     public $enableCsrfValidation = false;
     public function behaviors() {
         return [
             'verbs' => [
--- controllers/Kpitype3Controller.php
+++ controllers/Kpitype3Controller.php
@@ -14,6 +14,7 @@ use yii\filters\VerbFilter;
  */
 class Kpitype3Controller extends Controller {
 
+     public $enableCsrfValidation = false;
     public function behaviors() {
         return [
             'verbs' => [
